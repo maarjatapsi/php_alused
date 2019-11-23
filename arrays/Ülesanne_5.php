ac2064f685fb49e5957a8067ba98adfe0ac018c5
Maarja Tapsi <maarja.tapsi@khk.ee>
2019-11-23 16:42:46 +0200

    Pallivise

--- arrays/Ülesanne_5.php
+++ arrays/Ülesanne_5.php
@@ -4,17 +4,30 @@ echo "<h1>Õppeained</h1>";
 $ained = array('Programmeerimine', 'Matemaatika', 'IT-süsteemide riistvara', 'Veebidisain', 'Andmebaasi alused');
 //sorteerimine
 sort($ained);
-$arrlength = count($ained);
-for($x = 0; $x < $arrlength; $x++) {
-    echo $ained[$x];
-    echo "<br>";
+foreach ($ained as $aine) {
+    echo $aine. "<br>";
 }
 echo "<hr>";
 //algab pallivise
 echo "<h1>Pallivise</h1>";
 $nimed = array('Martin', 'Hardi', 'Juhan', 'Tiina', 'Sirje', 'Kaie');
 $pallivisked = array(33, 32, 27, 11, 15, 28);
-echo "Osalejate arv: " .count($nimed);
+//osalejaid kokku
+$o_kokku = count($nimed);
+//tulemus kokku
+$p_kokku = array_sum($pallivisked);
+//keskmine tulemus
+$p_keskmine = round($p_kokku / $o_kokku);
+//parim  tulemus
+$max = max($pallivisked);
+echo "Osalejate arv: " .$o_kokku;
+echo "<br>";
+echo "Keskmine palliviske tulemus: " .$p_keskmine;
+echo "<br>";
+echo "Parim tulemus: " .$max;
+echo "<br>";
+// otsime arrayed, mis käivad kokku
+echo "Parima tulemuse visanud õpilase nimi: " .$nimed[array_search($max, $pallivisked)];
 echo "<br>";
 
 echo "<hr>";
