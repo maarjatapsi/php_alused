3c2e9846f2cad44efdc5be3a27fdbb30ddb0a4cc
Maarja Tapsi <maarja.tapsi@khk.ee>
2019-11-23 15:35:01 +0200

    Töötamine massiivi funktsioonidega

--- arrays/massiivid.php
+++ arrays/massiivid.php
@@ -28,3 +28,45 @@ echo "<br><br>";
 foreach($hinded as $hinnang => $hinne) {
     echo "<li>Hinne: $hinne ($hinnang)</li>";
 }
+
+echo "<br><br>";
+//lisamine massiivi
+array_push($nimed, 'ahmed', 'ahti');
+var_dump($nimed);
+
+echo "<br><br>";
+$nimed[] = 'ahmed';
+var_dump($nimed);
+
+echo "<br><br>";
+$nimed[13] = 'bob';
+var_dump($nimed);
+echo "<br><br>";
+//lisab lgusesse
+array_unshift($nimed, 'laura');
+var_dump($nimed);
+//massiivist viimase väärtuse eemaldamine
+array_pop($nimed);
+//esimese väärtuse eemaldamine
+array_shift($nimed);
+$nimi = array_pop($nimed);
+echo "Eemaldati: ".$nimi;
+echo "<br><br>";
+//mitu väärtust massiivist hoitakse
+echo count($nimed);
+echo "<br><br>";
+// sorteerimine
+sort($nimed);
+var_dump($nimed);
+echo "<br><br>";
+//assotatiivne massiiv, sorteeritakse võtme järgi
+ksort($hinded);
+var_dump($hinded);
+echo "<br><br>";
+//assotatiivne massiiv, sorteeritakse väärtuse järgi
+asort($hinded);
+var_dump($hinded);
+echo "<br><br>";
+//tulemuse sassi ajamiseks
+shuffle($nimed);
+var_dump($nimed);
