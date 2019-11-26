47a3d69daaeb071d367a51931c8fe21bb1c38745
Maarja Tapsi <maarja.tapsi@khk.ee>
2019-11-26 09:05:59 +0200

    Vormitöötlus - vormi andmete käsitlus

--- forms/tellimine.php
+++ forms/tellimine.php
@@ -2,3 +2,17 @@
 ?>
 
 <h1>Tellimine</h1>
+<?php
+// serveri andmete kotroll
+//    var_dump($_GET);
+//    echo '<pre>';
+//print_r($_GET);
+//echo '</pre>';
+//lisab vormist saadud andmed muutujasse
+$toode1 = $_GET['t1'];
+$toode2 = $_GET['t2'];
+$toode3 = $_GET['t3'];
+
+echo 'Toode 1: '.$toode1.'tk<br>';
+echo 'Toode 2: '.$toode2.'tk<br>';
+echo 'Toode 3: '.$toode3.'tk<br>';
