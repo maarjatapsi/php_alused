8a0d64f0799a53eb6c20de150995d93fb70d8464
Maarja Tapsi <maarja.tapsi@khk.ee>
2019-11-26 08:46:29 +0200

    Vormitöötlus - algus vorm ja töötlus fail

---
+++ forms/vorm.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>06 - PHP - Vormid</title>
+</head>
+<body>
+<h1>Pood OÜ</h1>
+<form action="tellimine.php" method="get">
+    Toode 1 <input type="text"><br>
+    Toode 2 <input type="text"><br>
+    Toode 3 <input type="text"><br>
+    <input type="submit" value="Saada">
+</form>
+</body>
+</html>
