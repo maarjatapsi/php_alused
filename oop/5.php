d45c4cf22916750278d61204d12b7e008229c5c1
Maarja Tapsi <maarja.tapsi@khk.ee>
2020-01-06 13:37:35 +0200

    OOP - Magic methods and constants unveiled

---
+++ oop/5.php
@@ -0,0 +1,23 @@
+<?php
+class Car{
+    private $model = '';
+
+    //__construct
+    public function __construct($model = null)
+    {
+        if($model)
+        {
+            $this-> model = $model;
+        }
+    }
+
+    public function getCarModel()
+    {
+        //We use the __CLASS__ magic constant in order to get the class name
+        return " The <b>" . __CLASS__ . "</b> model is: " . $this -> model;
+    }
+}
+//We pass the value of the variable once we create the object
+$car1 = new Car("Mercedes");
+
+echo  $car1 -> getCarModel();
