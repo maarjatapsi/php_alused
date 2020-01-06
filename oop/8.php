c1df3e93003ca5d8c314b0ee6b124b81fe04ddf6
Maarja Tapsi <maarja.tapsi@khk.ee>
2020-01-06 14:35:38 +0200

    OOP - Interfaces - the next level of abstraction

---
+++ oop/8.php
@@ -0,0 +1,46 @@
+<?php
+interface interfaceName {
+    // abstract methods
+}
+
+class Child implements interfaceName {
+    // defines the interface methods and may have its own code
+}
+
+interface Car {
+    public function setModel($name);
+
+    public function getModel();
+}
+
+
+class miniCar implements Car, Vehicle {
+    private $model;
+    private $hasWheels;
+
+    public function setModel($name)
+    {
+        $this -> model = $name;
+    }
+
+    public function getModel()
+    {
+        return $this -> model;
+    }
+
+    public function setHasWheels($bool)
+    {
+        $this -> hasWheels = $bool;
+    }
+
+    public function getHasWheels()
+    {
+        return ($this -> hasWheels)? "has wheels" : "no wheels";
+    }
+}
+
+interface Vehicle {
+    public function setHasWheels($bool);
+
+    public function getHasWheels();
+}
